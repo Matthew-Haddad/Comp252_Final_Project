27e7a2b25dcee9021d4f6deec716886542cc30a3
ringnino <54418360+ringnino@users.noreply.github.com>
2019-12-11 18:26:57 -0800

    Update Order_System.h

--- Order_System.h
+++ Order_System.h
@@ -1,5 +1,4 @@
 #pragma once
-#pragma warning(disable:4996)
 #include <iostream>
 #include <iomanip>
 #include <string>
@@ -11,46 +10,20 @@
 
 int userChoice;
 int tableCounter = 1;
-void open_order_system_options();
-void orderSystemStartMenu();
 
 /*PROTOTYPES*/
 /*************************************************************************************************************************************************************/
-/*MAIN MENU SECTION*/
-void orderSystemMenu();
-
-/*ADD NEW ORDER SECTION*/
-Order newOrderToGo();					 /*Add a order without defining a table (Take-Out Order)*/
-Table newOrderTable();                    /*Add a new order to a table*/
-Order addToExistingOrder();               /*Add a suborder to an existing order*/
 
 /*VIEW OPTIONS SECTION*/
 void viewActiveSubOrder();               /*view active sub order*/
-void viewOptionalIngredients();          /*View optional ingredients in a meal*/
-void viewCookingPreferences();           /*view cooking preferences of ingredients that can be cooked different ways*/
 void viewAllSubOrders();                 /*view the status of all active suborders*/
-void viewTransactionList();              /*view the transaction list of current order*/
 void viewActiveMenu();                   /*only view the active menu*/
-
+void viewTransactionList();
 /*PRINT OPTIONS SECTION*/
 void printFinalReceipts();               /*print customer receipts on order completion*/
 
-/*DELETE OPTIONS SECTION*/
-void deleteSubOrder();                /*Delete a suborder that has not yet been confirmed within the order system*/
-void deleteTransactionList();         /*Delete an item or clear the transaction list*/
 
-/*ALERTS*/
-void drinksAlert(); /*Alert waiter when the drinks are complete*/
 /**************************************************************************************************************************************************************/
-
-
-
-/********************************************************************ADD SECTION*******************************************************************************/
-
-
-
-
-
 
 
 /********************************************************************VIEW SECTION******************************************************************************/
@@ -85,13 +58,28 @@ void viewAllSubOrders()									// USED TO VIEW ALL SUBORDERS
 
 }
 
+void viewTransactionList()
+{
+	std::cout << "************************************************************************************\n";
+	std::cout << "*" << std::setw(50) << "TRANSACTION LIST ORDERS" << std::setw(28) << "*\n";
+	std::cout << "************************************************************************************\n";
+	std::cout << "NAME" << std::setw(40) << "UNIT COST" << std::setw(24) << "PRICE" << "\n";
+}
 
 
 
 
 /********************************************************************PRINT SECTION*******************************************************************************/
-/********************************************************************DELETE SECTION*******************************************************************************/
-/********************************************************************ALERT SECTION*******************************************************************************/
+void printFinalReceipts()               /*print customer receipts on order completion*/
+{
+	std::cout << "******************************************\n";
+	std::cout << "*" << std::setw(25) << "FINAL RECEIPT" << std::setw(17) << "*\n";
+	std::cout << "******************************************\n";
+	std::cout << "\n";
+	std::cout << "NAME" << std::setw(35) << "SUB TOTAL\n";
+
+}
+
 
 
 
